20a0a17f09fb2f2c780fc6cbb470e5f9c9d9f837
0xstarcat <xxStarcat@gmail.com>
2020-04-18 16:43:16 -0400

    debug keys

--- src/scripts/InputManager/index.js
+++ src/scripts/InputManager/index.js
@@ -4,48 +4,44 @@ export default class InputManager {
   }
 
   enableActorMoveKeys(mapActor) {
-    this.scene.input.keyboard.on("keydown_A", (event) => {
-      mapActor.moveLeft();
+    this.scene.input.keyboard.on("keydown_W", (event) => {
+      mapActor.moveUp();
     });
 
     this.scene.input.keyboard.on("keydown_D", (event) => {
       mapActor.moveRight();
     });
 
-    this.scene.input.keyboard.on("keydown_W", (event) => {
-      mapActor.moveUp();
-    });
-
     this.scene.input.keyboard.on("keydown_S", (event) => {
       mapActor.moveDown();
     });
 
-    this.scene.input.keyboard.off("keydown_S", (event) => {
-      mapActor.moveDown();
-    });
-
-    this.scene.input.keyboard.on("keydown_LEFT", (event) => {
+    this.scene.input.keyboard.on("keydown_A", (event) => {
       mapActor.moveLeft();
-    });
-
-    this.scene.input.keyboard.on("keydown_RIGHT", (event) => {
-      mapActor.moveRight();
     });
 
     this.scene.input.keyboard.on("keydown_UP", (event) => {
       mapActor.moveUp();
     });
 
+    this.scene.input.keyboard.on("keydown_RIGHT", (event) => {
+      mapActor.moveRight();
+    });
+
     this.scene.input.keyboard.on("keydown_DOWN", (event) => {
       mapActor.moveDown();
+    });
+
+    this.scene.input.keyboard.on("keydown_LEFT", (event) => {
+      mapActor.moveLeft();
     });
   }
 
   resetActorMoveKeys() {
-    this.scene.input.keyboard.off("keydown_A");
-    this.scene.input.keyboard.off("keydown_S");
+    this.scene.input.keyboard.off("keydown_W");
     this.scene.input.keyboard.off("keydown_D");
-    this.scene.input.keyboard.off("keydown_F");
+    this.scene.input.keyboard.off("keydown_S");
+    this.scene.input.keyboard.off("keydown_A");
     this.scene.input.keyboard.off("keydown_UP");
     this.scene.input.keyboard.off("keydown_RIGHT");
     this.scene.input.keyboard.off("keydown_DOWN");
